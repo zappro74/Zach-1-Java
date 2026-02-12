ededb034cf6ccb5c64e8d57f62bf9c1a8b7867e8
zappro74 <150842322+zappro74@users.noreply.github.com>
2026-02-11 18:44:07 -0600

    Added Code

---
+++ Program.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Program
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose your own adventure!");
+
+        System.out.println("You wake up in the city of Albuquerque, New Mexico.");
+        System.out.println("Do you: ");
+        System.out.println("1. break bad.");
+        int choice = input.nextInt();
+
+        switch (choice)
+        {
+            case 1:
+                
+                break;
+        }
+
+    }
+}
